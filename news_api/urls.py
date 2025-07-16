65cbd8d84e9940002c0ae53989e4c2b6657c51d6
rohit rawat <rohit.rawatcontact@gmail.com>
2025-07-16 10:30:49 +0530

    added register in frontend

--- news_api/urls.py
+++ news_api/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/news/', include('news.urls')),
     path('', frontend),
+    path('api/user/', include('user.urls')),
 
 ]
