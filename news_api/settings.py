4e9a160b2d21a63d70bc8e1b8c818e3fe420368b
rohit03rawat <156593914+rohit03rawat@users.noreply.github.com>
2025-10-28 13:42:44 +0530

    db

--- news_api/settings.py
+++ news_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 from dotenv import load_dotenv
